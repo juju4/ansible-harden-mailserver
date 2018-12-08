3d3a2b2a55452240a3a4811cd6882fd48400ddb2
juju4 <juju4@users.noreply.github.com>
2018-12-08 06:34:15 -0500

    galaxy lint

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,7 +12,7 @@ fi
 #[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.harden-mailserver ] && ln -s ansible-harden-mailserver $rolesdir/juju4.harden-mailserver
-[ ! -e $rolesdir/juju4.harden-mailserver ] && cp -R $rolesdir/ansible-harden-mailserver $rolesdir/juju4.harden-mailserver
+[ ! -e $rolesdir/juju4.harden_mailserver ] && cp -R $rolesdir/ansible-harden-mailserver $rolesdir/juju4.harden_mailserver
 
 ## don't stop build on this script return code
 true
