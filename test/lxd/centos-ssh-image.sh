a6c0ed4945678895782fcb10df96da56268ec9cf
juju4 <juju4@users.noreply.github.com>
2019-10-19 07:23:33 -0400

    test/lxd: centos8 variant

--- test/lxd/centos-ssh-image.sh
+++ test/lxd/centos-ssh-image.sh
@@ -16,8 +16,15 @@ openssl rand -base64 48 | perl -ne 'print "$_" x2' | lxc exec $guest -- passwd r
 
 lxc exec $guest -- dhclient eth0
 lxc exec $guest -- ping -c 1 8.8.8.8
-lxc exec $guest -- yum -y upgrade
-lxc exec $guest -- yum install -y openssh-server sudo ruby yum-utils
+if [ "X$1" = "X8" ]; then
+  lxc exec $guest -- dnf -y upgrade
+  lxc exec $guest -- dnf install -y openssh-server sudo ruby yum-utils
+  lxc exec $guest -- dnf install -y python3 python3-pip openssl-devel python36-devel libffi-devel "@Development tools"
+  lxc exec $guest -- ln -s /usr/bin/pip3 /usr/bin/pip
+else
+  lxc exec $guest -- yum -y upgrade
+  lxc exec $guest -- yum install -y openssh-server sudo ruby yum-utils
+fi
 lxc exec $guest -- systemctl enable sshd
 lxc exec $guest -- systemctl start sshd
 lxc exec $guest -- mkdir /root/.ssh || true
